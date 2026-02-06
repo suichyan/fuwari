86e6e00c77c497882c0c7ce3a61663ff09c20716
suichyan <anxiao133@gmail.com>
2026-02-07 01:52:17 +0800

    配置github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.isyyo.com/",
-	base: "/",
+	site: "https://suichyan.github.io",
+	base: "/fuwari",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
