57886334ae24f1a224cf5b8d40a47fb941ddad6a
suichyan <anxiao133@gmail.com>
2026-02-07 14:40:48 +0800

    设置域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://suichyan.github.io",
-	base: "/fuwari",
+	site: "https://blog.suichyan.top",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
