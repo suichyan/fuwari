5f5b0b693ea3cafb23182896eeaec89ac60edf8c
suichyan <anxiao133@gmail.com>
2026-02-17 20:02:58 +0800

    添加sakuya的友链

--- src/friends_data.ts
+++ src/friends_data.ts
@@ -14,4 +14,12 @@ export const friends: Friend[] = [
     avatar: "https://blog.openyq.top/image/favicon.png",
     description: "愿世间所有人都被温柔以待❤",
   },
+
+  {
+    name: "咲夜の小屋",
+    url: "https://sakuya.moe/",
+    avatar: "https://bu.dusays.com/2024/10/23/671837f2232c2.png",
+    description: "这里有笨比，大聪明以及sakuya~！",
+  }
 ];
+
