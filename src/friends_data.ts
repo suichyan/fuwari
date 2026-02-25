dd7626b485e98b21deb34e1157566d410be7791c
suichyan <anxiao133@gmail.com>
2026-02-25 21:41:58 +0800

    更换冰华友链

--- src/friends_data.ts
+++ src/friends_data.ts
@@ -8,24 +8,25 @@ export interface Friend {
 
 // 友链数据
 export const friends: Friend[] = [
-  {
-    name: "YQ's Toy Box",
-    url: "https://blog.openyq.top/",
-    avatar: "https://blog.openyq.top/image/favicon.png",
-    description: "愿世间所有人都被温柔以待❤",
-  },
+	{
+		name: "YQ's Toy Box",
+		url: "https://blog.openyq.top/",
+		avatar: "https://blog.openyq.top/image/favicon.png",
+		description: "愿世间所有人都被温柔以待❤",
+	},
 
-  {
-    name: "咲夜の小屋",
-    url: "https://sakuya.moe/",
-    avatar: "https://bu.dusays.com/2024/10/23/671837f2232c2.png",
-    description: "这里有笨比，大聪明以及sakuya~！",
-  },
-  {
-    name: "冰华的下午茶室",
-    url: "https://huloud.kesug.com/",
-    avatar: "https://huloud.kesug.com/wp-content/uploads/2026/02/IMG_0896_compressed.jpg",
-    description: "永远保持好奇心~",
-  }
+	{
+		name: "咲夜の小屋",
+		url: "https://sakuya.moe/",
+		avatar: "https://bu.dusays.com/2024/10/23/671837f2232c2.png",
+		description: "这里有笨比，大聪明以及sakuya~！",
+	},
+	{
+		name: "冰华的下午茶室",
+		url: "https://blog.huloud.top/",
+		avatar:
+			"https://github.com/Fdsfccffc/Fdsfccffc/blob/main/IMG_1388.JPG?raw=true",
+		description: "永远保持好奇心~",
+	},
 ];
 
